ededb7a107ed59b215ba52f8b63b8edac780452d
shashwat-knol <94095183+shashwat-knol@users.noreply.github.com>
2021-12-05 21:16:55 +0530

    Add files via upload

---
+++ src/main/scala/com/knoldus/db/Empdata.scala
@@ -0,0 +1,5 @@
+package com.knoldus.db
+import  com.knoldus.models.Employee
+class Empdata {
+  val employee:Employee = Employee("Shashwat","Singh",24,12000,"Intern","Knoldus","shashwat.singh@knoldus.com")
+}
